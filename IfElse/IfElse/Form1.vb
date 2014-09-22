63330804ebf0e76c83b696a213e66cb85e731588
idsc3101 <idsc3101@gmail.com>
2014-09-22 15:22:41 -0500

    Solved

--- IfElse/IfElse/Form1.vb
+++ IfElse/IfElse/Form1.vb
@@ -4,16 +4,23 @@ Public Class Form1
 
     Private Sub btnFindLarger_Click(sender As System.Object, e As System.EventArgs) Handles btnFindLarger.Click
         Dim num1, num2, largerNum As Double
+             ' Modify the program such that it does not crash 
+            ' if the user types incorrect input
 
-        ' Modify the program such that it does not crash 
-        ' if the user types incorrect input
+            Dim st1 As String = txtFirstNum.Text
+            Dim st2 As String = txtSecondNum.Text
 
-        Dim st1 As String = txtFirstNum.Text
-        Dim st2 As String = txtSecondNum.Text
+        If Not (IsNumeric(st1) And IsNumeric(st2)) Then
+            MessageBox.Show("Check 1:Please type the numbers")
+            Return
+        End If
 
         num1 = CDbl(st1)
         num2 = CDbl(st2)
 
+        If Not (num1 >= 0 And num2 >= 0) Then
+            MessageBox.Show("Check 2: Please not type negative numbers")
+        End If
         Dim result As String
 
         If num1 > num2 Then
@@ -21,10 +28,17 @@ Public Class Form1
         ElseIf num2 > num1 Then
             result = "The larger number is " & num2
         Else
-            result = "They are equal"
-        End If
 
+
+            result = "They are equal"
+
+        End If
         txtResult.Text = result
+
+
     End Sub
 
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
